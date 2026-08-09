5d2113503d7097001a2e801591e71cc1411aa955
Laxman Singh <laxman@novvr.com>
2026-08-09 10:02:38 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body suppressHydrationWarning className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-GBDJ96C34C" />
       </body>
     </html>
   );
